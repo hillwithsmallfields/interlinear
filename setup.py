77891839cef048b2c6e0798434fbc49249e04940
John Sturdy <jcg.sturdy@gmail.com>
2026-04-01 16:23:06 +0100

    add project files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="interlinear",
+    version="0.0.1",
+    description="Interlinear text system",
+    author="John C. G. Sturdy",
+    author_email="jcg.sturdy@gmail.com",
+    packages=find_packages(),
+    install_requires=['expressionive', 'orgbookchapterverse']
+)
