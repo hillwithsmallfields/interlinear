b0fde14a081ba0dfd43ea1da05698b187f784780
John Sturdy <jcg.sturdy@gmail.com>
2026-06-15 10:06:45 +0100

    bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="interlinear",
-    version="0.0.1",
+    version="0.0.2",
     description="Interlinear text system",
     author="John C. G. Sturdy",
     author_email="jcg.sturdy@gmail.com",
