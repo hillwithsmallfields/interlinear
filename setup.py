03069c8520697bafcd24be6d29d886f6f8ad8f87
John Sturdy <jcg.sturdy@gmail.com>
2026-08-30 21:49:03 +0100

    Various packaging-related fixes

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="interlinear",
-    version="0.0.2",
+    version="0.1.1",
     description="Interlinear text system",
     author="John C. G. Sturdy",
     author_email="jcg.sturdy@gmail.com",
-    packages=find_packages(),
     install_requires=['expressionive', 'orgbookchapterverse']
 )
